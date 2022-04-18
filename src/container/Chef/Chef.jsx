de72fa990e527495aed71b67023a0be5fb325581
wilsonibekason <wanbekgithub@gmail.com>
2022-04-18 16:02:56 -0700

    completed chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import {images} from '../../constants'
+import { SubHeading } from '../../components';
 
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title='Chef"s world '/>
+      <h1 className="headtext__cormorant">
+        What we believe in
+      </h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, autem!</p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae aspernatur voluptatibus minus quod?
+        </p>
+      </div>
+      <div className="app__chef-sign">
+        <p className="p__opensans">Wilson Ibekason</p>
+        <p className="p__opensans">Chief and founder</p>
+        <img src={images.sign} alt="sign" />
+        </div>
+    </div>
   </div>
 );
 
