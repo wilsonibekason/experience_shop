06bc7dab6a0a72cef7d1b2fb3da773e3f68d206b
wilsonibekason <wanbekgithub@gmail.com>
2022-04-19 09:41:30 -0700

    still working on footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { SubHeading} from '../../components'
 
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'  style={{marginTop: '13rem'}}>
+    <div className="app__newsletter-heading">
+      <SubHeading title='newsletter' />
+      <h1 className="headtext__cormorant" style={{textAlign: 'center'}}>
+        Subscribe to our Newsletter
+      </h1>
+      <p className="p__opensans">
+        Lorem, ipsum dolor.
+      </p>
+    </div>
+
+    <div className="app__newsletter-input flex__center">
+      <input type="text" name='email' placeholder='Enter Your Email Address' />
+      <button className="custom__button">
+        Subscribe
+      </button>
+    </div>
   </div>
 );
 
 export default Newsletter;
+ 
