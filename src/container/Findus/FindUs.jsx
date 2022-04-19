79333751f80d7d05d59181bee9dae2b2487b13ec
wilsonibekason <wanbekgithub@gmail.com>
2022-04-19 08:57:58 -0700

    completed footer section

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { SubHeading,  } from '../../components';
+import { images } from '../../constants';
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div className="app__wrapper_info">
+        <SubHeading title='contact' />
+        <h1 className='headtext__cormorant' style={{marginBottom: '3rem'}}> Find Us</h1>
+        <div className="app__wrapper-content">
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, magni!
+          </p>
+          <p className="p__cormorant" style={{color: '#DDD68', margin: '2rem 0'} }>
+            Lorem ipsum 
+          </p>
+          <p className="p__opensans">
+            Lorem ipsum dolor 
+          </p>
+          <p className="p__opensans">
+            Lorem ipsum dolor 
+          </p>
+        </div>
+        <button className='custom__button' style={{marginBottom: '2rem'}}> </button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 
